5ad29a1fe5a2b671478340c4897eb4d59d2f28ac
hispanicsanic <76631426+hispanicsanic@users.noreply.github.com>
2021-07-25 12:00:54 +0100

    Add files via upload

--- config.h
+++ config.h
@@ -9,8 +9,8 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#D8DEE9", "#282828" },
-	[SchemeSel] = { "#282828", "#ffd1dc" },
+	[SchemeNorm] = { "#D8DEE9", "#2E3440" },
+	[SchemeSel] = { "#2E3440", "#ffd1dc" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
